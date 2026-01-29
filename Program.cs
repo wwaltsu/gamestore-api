64ea462a6de21d7338c5a62e3ad40aca3064e128
waltsu <waltsumarcus@gmail.com>
2026-01-29 03:16:50 +0200

    feat: add EF Core DbContext and initial SQLite migration

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-using GameStore.Api.Dtos;
+using GameStore.Api.Data;
 using GameStore.Api.Endpoints;
 
 
@@ -6,7 +6,13 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddValidation();
 
+var connString = "Data Source=GameStore.db";
+builder.Services.AddSqlite<GameStoreContext>(connString);
+
 var app = builder.Build();
 
 app.MapGameEndpoints();
+
+app.MigrateDb();
+
 app.Run();
